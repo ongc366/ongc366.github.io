16db74612882ec959c6d045fa53d52d6070bd9fe
ongc366 <ongc366@newschool.edu>
2015-09-20 17:12:34 +0000

    Working on showing popups

---
+++ notafolder/studio/spacedata/spaceFinal/script.js
@@ -0,0 +1,9 @@
+function popUp() {
+    var x;
+    if (confirm("Press a button!") == true) {
+        x = "You pressed OK!";
+    } else {
+        x = "You pressed Cancel!";
+    }
+    document.getElementById("demo").innerHTML = x;
+}
