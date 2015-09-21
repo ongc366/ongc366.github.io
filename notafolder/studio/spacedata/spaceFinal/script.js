bb0fdde3dac691a71b6015cd96bd38cbc77a8bd7
ongc366 <ongc366@newschool.edu>
2015-09-21 16:21:46 +0000

    popup window availability

---
+++ notafolder/studio/spacedata/spaceFinal/script.js
@@ -0,0 +1,10 @@
+function popUp() {
+    window.open("assets/page1GIF/page1GIFA.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=100, width=300, height=300");
+    window.open("assets/page1GIF/page1GIFB.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=300, width=300, height=300");
+    //window.open("assets/page1GIF/page1GIFC.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=20, left=100, width=300, height=300");
+    //window.open("assets/page1GIF/page1GIFD.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=200, left=300, width=300, height=300");
+    //window.open("assets/page1GIF/page1GIFE.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=20, left=100, width=300, height=300");
+    //window.open("assets/page1GIF/page1GIFF.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=200, left=300, width=300, height=300");
+    //window.open("assets/page1GIF/page1GIFG.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=20, left=100, width=300, height=300");
+}
+
