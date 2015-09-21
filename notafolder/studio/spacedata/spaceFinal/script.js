5f10e5583312f615d922ea17daad0b41a95716a7
ongc366 <ongc366@newschool.edu>
2015-09-21 04:37:11 +0000

    edited css

--- notafolder/studio/spacedata/spaceFinal/script.js
+++
@@ -1,9 +0,0 @@
-function popUp() {
-    var x;
-    if (confirm("Press a button!") == true) {
-        x = "You pressed OK!";
-    } else {
-        x = "You pressed Cancel!";
-    }
-    document.getElementById("demo").innerHTML = x;
-}
