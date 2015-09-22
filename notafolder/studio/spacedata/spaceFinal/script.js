733f4d5b8b60cd36e9281e69f7ca7ed1edc2f18c
ongc366 <ongc366@newschool.edu>
2015-09-22 21:15:41 +0000

    working wireframe update

--- notafolder/studio/spacedata/spaceFinal/script.js
+++ notafolder/studio/spacedata/spaceFinal/script.js
@@ -4,7 +4,13 @@ function popUp() {
 }
 
 function intro() {
-    window.open("https://www.google.com/maps/place/Av+C%2FE+16+St/@40.729431,-73.9746577,18z/data=!4m2!3m1!1s0x89c25973d107dfdb:0x432f009c2ef56a3cl", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=100, width=400, height=400");
+    window.open("https://www.google.com/maps/place/Av+C%2FE+16+St/@40.729431,-73.9746577,18z/data=!4m2!3m1!1s0x89c25973d107dfdb:0x432f009c2ef56a3cl", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=500, width=400, height=400");
+}
+
+function sliding() {
+    setTimeout(function(){
+        window.open("shadow1.html", "slideshow", "toolbar=no, scrollbars=no, resizable=no, top=20, left=100, width=300, height=300");
+        }, 3000);
 }
 
 
