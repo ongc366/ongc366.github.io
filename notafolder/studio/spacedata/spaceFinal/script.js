07b69c931374a8b3c8b1fb4ec445bc6af9f8d0d9
ongc366 <ongc366@newschool.edu>
2015-09-22 17:45:42 +0000

    finished styles objective

--- notafolder/studio/spacedata/spaceFinal/script.js
+++ notafolder/studio/spacedata/spaceFinal/script.js
@@ -1,11 +1,10 @@
 function popUp() {
     window.open("assets/page1GIF/page1GIFA.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=100, width=300, height=300");
     window.open("assets/page1GIF/page1GIFB.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=300, width=300, height=300");
-    //window.open("assets/page1GIF/page1GIFC.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=20, left=100, width=300, height=300");
-    //window.open("assets/page1GIF/page1GIFD.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=200, left=300, width=300, height=300");
-    //window.open("assets/page1GIF/page1GIFE.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=20, left=100, width=300, height=300");
-    //window.open("assets/page1GIF/page1GIFF.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=200, left=300, width=300, height=300");
-    //window.open("assets/page1GIF/page1GIFG.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=20, left=100, width=300, height=300");
+}
+
+function intro() {
+    window.open("https://www.google.com/maps/place/Av+C%2FE+16+St/@40.729431,-73.9746577,18z/data=!4m2!3m1!1s0x89c25973d107dfdb:0x432f009c2ef56a3cl", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=100, width=400, height=400");
 }
 
 
