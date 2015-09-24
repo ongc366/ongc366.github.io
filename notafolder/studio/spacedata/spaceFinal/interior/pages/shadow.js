4f3bfbdc6d3061bd17e75a79959e9c843caa68ad
ongc366 <ongc366@newschool.edu>
2015-09-24 21:42:20 +0000

    edited intro pages

--- notafolder/studio/spacedata/spaceFinal/interior/pages/shadow.js
+++ notafolder/studio/spacedata/spaceFinal/interior/pages/shadow.js
@@ -8,8 +8,6 @@ function popUp1() {
 
 function popUp2() {
     window.open("assets/page1GIF/page1GIFA.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=100, width=300, height=300");
-    window.open("assets/page1GIF/page1GIFA.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=100, width=300, height=300");
-    window.open("assets/page1GIF/page1GIFB.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=300, width=300, height=300");
     window.open("assets/page1GIF/page1GIFB.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=300, width=300, height=300");
 }
 
@@ -29,7 +27,7 @@ function popUp5() {
 }
 
 function intro() {
-    window.open("https://www.google.com/maps/place/Av+C%2FE+16+St/@40.729431,-73.9746577,18z/data=!4m2!3m1!1s0x89c25973d107dfdb:0x432f009c2ef56a3cl", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=500, width=400, height=400");
+    window.open("https://www.google.com/maps/place/Av+C%2FE+16+St/@40.729431,-73.9746577,18z/data=!4m2!3m1!1s0x89c25973d107dfdb:0x432f009c2ef56a3cl", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=500, width=500, height=600");
     window.open("introMessage.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=300, left=810, width=200, height=350");
 }
 
