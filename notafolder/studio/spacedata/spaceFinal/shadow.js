42e230d6ad8a4126b9804da25985861e963cbd33
ongc366 <ongc366@newschool.edu>
2015-09-24 21:54:53 +0000

    intro update 2

--- notafolder/studio/spacedata/spaceFinal/shadow.js
+++ notafolder/studio/spacedata/spaceFinal/shadow.js
@@ -1,15 +1,41 @@
-function popUp() {
+function popUp1() {
+    window.open("nature1.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=300, left=300, width=400, height=300");
+    window.open("nature4.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=700, width=400, height=400");
+    window.open("nature2.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=50, left=200, width=300, height=300");
+    window.open("nature3.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=100, left=500, width=300, height=300");
+    window.open("natureSound.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=550, left=700, width=400, height=20");
+}
+
+function popUp2() {
+    window.open("assets/page1GIF/page1GIFA.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=100, width=300, height=300");
+    window.open("assets/page1GIF/page1GIFB.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=300, width=300, height=300");
+}
+
+function popUp3() {
+    window.open("assets/page1GIF/page1GIFA.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=100, width=300, height=300");
+    window.open("assets/page1GIF/page1GIFB.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=300, width=300, height=300");
+}
+
+function popUp4() {
+    window.open("assets/page1GIF/page1GIFA.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=100, width=300, height=300");
+    window.open("assets/page1GIF/page1GIFB.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=300, width=300, height=300");
+}
+
+function popUp5() {
     window.open("assets/page1GIF/page1GIFA.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=100, width=300, height=300");
     window.open("assets/page1GIF/page1GIFB.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=300, width=300, height=300");
 }
 
 function intro() {
-    window.open("https://www.google.com/maps/place/Av+C%2FE+16+St/@40.729431,-73.9746577,18z/data=!4m2!3m1!1s0x89c25973d107dfdb:0x432f009c2ef56a3cl", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=500, width=400, height=400");
+    window.open("https://www.google.com/maps/place/Av+C%2FE+16+St/@40.729431,-73.9746577,18z/data=!4m2!3m1!1s0x89c25973d107dfdb:0x432f009c2ef56a3cl", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=500, width=500, height=600");
     window.open("introMessage.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=300, left=810, width=200, height=350");
 }
+
 
 /*function sliding() {
     setTimeout(function(){
         window.open("shadow1.html", "slideshow", "toolbar=no, scrollbars=no, resizable=no, top=20, left=100, width=300, height=300");
         }, 3000);
 }*/
+
+
