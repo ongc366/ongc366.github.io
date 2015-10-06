a5f511c11d0eb65fa45ee541cad99cead66e5bc0
ongc366 <ongc366@newschool.edu>
2015-10-06 20:38:43 +0000

    reuploading 3 gif constructions

--- root/spaceFinal/assets/script/shadow.js
+++ root/spaceFinal/assets/script/shadow.js
@@ -20,7 +20,6 @@ function popUp3() {
 
 function popUp4() {
     window.open("assets/page1GIF/page1GIFA.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=100, width=300, height=300");
-    window.open("assets/page1GIF/page1GIFB.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=300, width=300, height=300");
 }
 
 function popUp5() {
