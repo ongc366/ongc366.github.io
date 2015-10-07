53dde438db9640919059786b0b356f3b4413e500
ongc366 <ongc366@newschool.edu>
2015-10-07 00:05:22 +0000

    slight shift

--- root/spaceFinal/assets/script/shadow.js
+++ root/spaceFinal/assets/script/shadow.js
@@ -31,7 +31,7 @@ function popUp5() {
 
 function intro() {
     window.open("https://www.google.com/maps/place/Av+C%2FE+16+St/@40.729431,-73.9746577,18z/data=!4m2!3m1!1s0x89c25973d107dfdb:0x432f009c2ef56a3cl", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=500, width=500, height=600");
-    window.open("introMessage.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=300, left=810, width=200, height=350");
+    window.open("introMessage.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=300, left=900, width=200, height=350");
 }
 
 
