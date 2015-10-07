5949faf13b28d458455cd6446fdd960c3824d838
ongc366 <ongc366@newschool.edu>
2015-10-07 17:23:46 +0000

    slight change to intro pop up size

--- root/spaceFinal/assets/script/shadow.js
+++ root/spaceFinal/assets/script/shadow.js
@@ -31,12 +31,5 @@ function popUp5() {
 
 function intro() {
     window.open("https://www.google.com/maps/place/Av+C%2FE+16+St/@40.729431,-73.9746577,18z/data=!4m2!3m1!1s0x89c25973d107dfdb:0x432f009c2ef56a3cl", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=500, width=500, height=600");
-    window.open("introMessage.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=300, left=900, width=200, height=350");
+    window.open("introMessage.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=320, left=900, width=250, height=350");
 }
-
-
-/*function sliding() {
-    setTimeout(function(){
-        window.open("shadow1.html", "slideshow", "toolbar=no, scrollbars=no, resizable=no, top=20, left=100, width=300, height=300");
-        }, 3000);
-}*/
