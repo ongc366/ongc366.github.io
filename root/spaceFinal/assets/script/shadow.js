4cc582fa6eac2eb30520eda706f5d9131f8787af
ongc366 <ongc366@newschool.edu>
2015-10-08 13:33:40 +0000

    update GIF Construction

--- root/spaceFinal/assets/script/shadow.js
+++ root/spaceFinal/assets/script/shadow.js
@@ -29,6 +29,56 @@ function popUp5() {
     window.open("../spaceFinal/assets/pages/leaving.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=275, width=100, height=100");
 }
 
+function popUp6() {
+    window.open("../spaceFinal/assets/images/popups/candle23.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle22.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle21.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle20.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle19.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle18.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle17.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle16.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle15.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");      
+    window.open("../spaceFinal/assets/images/popups/candle14.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle13.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle12.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle11.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle10.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle9.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes,  top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle8.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes,  top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle7.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes,  top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle6.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes,  top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle5.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes,  top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle4.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes,  top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle3.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes,  top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle2.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes,  top=200, left=450, width=300, height=300");
+    window.open("../spaceFinal/assets/images/popups/candle1.jpg", "_blank", "toolbar=no, scrollbars=no, resizable=yes,  top=200, left=450, width=300, height=300");
+}
+
+/*function popUp7() {
+    window.open("../spaceFinal/assets/pages/space.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=100, left=450, width=300, height=500");
+}
+
+function popUp8() {
+    window.open("../spaceFinal/assets/pages/flyingsouth.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=200, left=360, width=500, height=300");
+}
+
+function popUp9() {
+    window.open("../spaceFinal/assets/pages/space.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=100, left=450, width=300, height=500");
+}
+
+function popUp10() {
+    window.open("../spaceFinal/assets/pages/space.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=100, left=450, width=300, height=500");
+}
+
+function popUp11() {
+    window.open("../spaceFinal/assets/pages/space.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=100, left=450, width=300, height=500");
+}
+
+function popUp12() {
+    window.open("../spaceFinal/assets/pages/space.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=100, left=450, width=300, height=500");
+}*/
+
 function intro() {
     window.open("https://www.google.com/maps/place/Av+C%2FE+16+St/@40.729431,-73.9746577,18z/data=!4m2!3m1!1s0x89c25973d107dfdb:0x432f009c2ef56a3cl", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=20, left=500, width=500, height=600");
     window.open("introMessage.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, top=320, left=900, width=250, height=350");
