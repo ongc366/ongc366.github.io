6032cb466cfcdaeba6a55668a92d168a77f20b46
ongc366 <ongc366@newschool.edu>
2015-10-14 22:06:15 +0000

    popups for work archive

---
+++ root/archive/archive.js
@@ -0,0 +1,78 @@
+function perla() {
+    window.open("http://perlah.github.io/Lab/composition2/index.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=600, width=500, height=800");
+}
+
+function lauraM() {
+    window.open("http://mortl839.github.io/flags2.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=650, width=500, height=800");
+}
+
+function kyung() {
+    window.open("http://kyungjunjang.github.io/Week2-1.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=700, width=500, height=800");
+}
+
+function haley() {
+    window.open("http://goldifish.github.io/myfolder/abstract.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=750, width=500, height=800");
+}
+
+function myles() {
+    window.open("http://myles101.github.io/Page1.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=800, width=500, height=800");
+}
+
+function allcolor(){
+    window.open("http://myles101.github.io/Page1.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=800, width=500, height=800");
+    window.open("http://goldifish.github.io/myfolder/abstract.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=750, width=500, height=800");
+    window.open("http://kyungjunjang.github.io/Week2-1.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=700, width=500, height=800");
+    window.open("http://mortl839.github.io/flags2.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=650, width=500, height=800");
+    window.open("http://perlah.github.io/Lab/composition2/index.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=600, width=500, height=800");
+}
+
+function mrinal() {
+    window.open("http://mrinalbhatia.github.io/lab/composition.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=650, width=500, height=800");
+}
+
+function charles() {
+    window.open("http://hsiat057.github.io/composition/composition.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=700, width=500, height=800");
+}
+
+function nikoletta() {
+    window.open("http://mitsn437.github.io/composition/mycomposition.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=750, width=500, height=800");
+}
+
+function clarissa() {
+    window.open("http://ongc366.github.io/root/compositiondata/composition3.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=800, width=500, height=800");
+}
+
+function allpaste(){
+    window.open("http://ongc366.github.io/root/compositiondata/composition3.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=800, width=500, height=800");
+    window.open("http://mitsn437.github.io/composition/mycomposition.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=750, width=500, height=800");
+    window.open("http://hsiat057.github.io/composition/composition.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=700, width=500, height=800");
+    window.open("http://mrinalbhatia.github.io/lab/composition.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=650, width=500, height=800");
+}
+
+function nicole() {
+    window.open("http://debln128.github.io/composition.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=600, width=500, height=800");
+}
+
+function carlos() {
+    window.open("http://hernc.github.io/compositions/Compositioncss.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=650, width=500, height=800");
+}
+
+function pedro() {
+    window.open("http://joffp019.github.io/Composition2/5elements_wood.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=700, width=500, height=800");
+}
+
+function lauraT() {
+    window.open("http://tienl199.github.io/Composition/composition.2.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=750, width=500, height=800");
+}
+
+function juju() {
+    window.open("http://xiejunlu.github.io/composition2.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=800, width=500, height=800");
+}
+
+function allsymbol(){
+    window.open("http://xiejunlu.github.io/composition2.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=800, width=500, height=800");
+    window.open("http://tienl199.github.io/Composition/composition.2.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=750, width=500, height=800");
+    window.open("http://joffp019.github.io/Composition2/5elements_wood.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=700, width=500, height=800");
+    window.open("http://hernc.github.io/compositions/Compositioncss.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=650, width=500, height=800");
+    window.open("http://debln128.github.io/composition.html", "_blank", "toolbar=no, scrollbars=no, resizable=yes, top=0, left=600, width=500, height=800");
+}
