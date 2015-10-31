a36409b517e9f5034a487e8625eec33331230f1f
ongc366 <ongc366@newschool.edu>
2015-10-31 00:22:49 +0000

    finalize pattern styles

---
+++ discoball/js/main.js
@@ -0,0 +1,28 @@
+
+$('p').html('hello world');
+
+//Math.random(), returns a random number, will not return a 0 or a 1, decimal return only
+//multiply to increase values of a Math.random(), multiply by the number of stuff in the array, i.e. if the number of stuff in the array is 3 then multiply Math.random() by 3
+//round down because javascript is 0 index Math.round(), rounds down
+//Math.ceil(), rounds up
+//Math.floor()
+//Math.floor(Math.random()*3   values in between 0 and two, randomly
+//$('body').html(<img src = 'img/cat1.jpg')
+//append, prepend
+//var randomCat = 
+//$('body').append(<img src = ' + randomCat + '>');
+//"<img src = 'img/" + randomCat + "'>"
+//$('.button').css('top')
+//$(window).height()-elementSize     $(window.width()-elementSize
+//subtract element size from total value of the screen
+//$(window).resize(function() {
+    
+//})
+//jquery loop: each
+//$('.button').each(function() {
+    
+//});
+//this $(this), sets elements for each instance of the loop equation
+//'#' + Math.random() * 16777215
+//$(iw)
+//.append(words[Math.random...etc)
