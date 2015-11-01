03394e0d19b737f71b52b02c6398a17f9921192a
ongc366 <ongc366@newschool.edu>
2015-11-01 22:01:26 +0000

    styles change

--- root/assets/script/script.js
+++ root/assets/script/script.js
@@ -1,32 +1,15 @@
 var clickedWho = false;
 var clickedWhat = false;
 
-$('.contact').addClass('back');
-
 $('.who').click(function() {
   if(clickedWho == false) {
     $('.who').css('width', '50%').css('height', '5%');
     $('.what').css('width', '50%');
     $('.left').removeClass('back');
-    $('.who').mouseenter(function() {
-      $('.who').css('height', '70%');
-      $('.who').mouseleave(function() {
-        $('.who').css('height', '5%');
-      })
-    })
     clickedWho = true;
   } else if (clickedWho == true){
     $('.who').css('height', '100%');
     $('.left').addClass('back');
-    $('.who').mouseenter(function() {
-      $('.who').css('height', '100%');
-      $('.who').mouseleave(function() {
-        $('.who').css('height', '100%');
-      })
-    })
-    $('.who').mouseleave(function() {
-        $('.who').css('height', '100%');
-      })
     clickedWho = false;
   }
 })
